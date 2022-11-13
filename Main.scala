b5ef55c20139aba3e8507d9a6c0f545d6c7b0483
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-13 09:26:45 -0800

    Add files via upload

---
+++ Main.scala
@@ -0,0 +1,14 @@
+// Marvish Chandra
+
+def petNeeds(need):
+    if need == "food":
+        print("Make sure that the dog or cat is feed the appropriate food type based on their size and weight.")
+    if need == "water":
+        print("Have a bowl of water set out for the dog or cat either indoors or outdoors wherever the pet is located.")
+    if need == "attention":
+        print("Based on the breed or type of the animal, make sure that their needs are met by spending time with them.")
+    if need == "urination and hygiene habits":
+        print("Have your pet trained to take care of themselves outside to avoid inside cleanup.")
+    if need == "general hygiene":
+        print("Routinely shower your pets at least once a week or once in two weeks based on how dirty your pet becomes.")
+    else: print("Determine your need of your pet by referencing these guidelines or your need is not recognized as the most important.")
